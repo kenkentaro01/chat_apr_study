4d60b5b366a607e9fa5f2b8023e1d7aebbbc1951
kentaro <k.moco0109@gmail.com>
2024-04-06 23:04:36 +0900

    チャットアプリの作成

---
+++ chat_apr_study/chat_apr_study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chat_apr_study
+//
+//  Created by 健太郎 on 2024/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chat_apr_study/chat_apr_study/chat_apr_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_apr_studyApp.swift
+//  chat_apr_study
+//
+//  Created by 健太郎 on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct chat_apr_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
